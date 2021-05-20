1430552ef57bb9e57db55ef21cdbd835314a9025
RanveerCoder <84279510+RanveerCoder@users.noreply.github.com>
2021-05-20 16:57:31 +0530

    SDMP School
    SDMP School

--- applynow.php
+++ applynow.php
@@ -1,39 +1,40 @@
 <?php
 
-$FatherName = $_POST['FatherName'];
-$MotherName = $_POST['MotherName'];
-$StudentName = $_POST['StudentName'];
-$DOB = $_POST['DOB'];
-$ParentEducation = $_POST['ParentEducation'];
-$Gender = $_POST['Gender'];
-$Applyingfor = $_POST['Applyingfor'];
-$AadharNo. = $_POST['AadharNo'];
-$PhoneNo. = $_POST['PhoneNo'];
+$Father_Name = $_POST['FatherName'];
+$Mother_Name = $_POST['MotherName'];
+$Student_Name = $_POST['StudentName'];
+$Dob = $_POST['DOB'];
+$Parent_Education = $_POST['ParentEducation'];
+$Gender_ = $_POST['Gender'];
+$Applying_for = $_POST['Applyingfor'];
+$AadharNo = $_POST['AadharNo.'];
+$PhoneNo = $_POST['PhoneNo.'];
 $visitor_email  = $_POST['EmailId'];
-$Address = $_POST['Address'];
+$Address_ = $_POST['Address'];
 
-$email_from = 'factpathshalaoficial@gmail.com';
+    $email_from = 'factpathshalaoficial@gmail.com';
+
 $email_subject = "New Addmission";
-$email_body = "Father's Name: $FatherName.\n".
-              "Mother's Name: $MotherName.\n".
-              "Student Name: $StudentName.\n".
-              "DOB: $DOB.\n".
-              "Parent's Education = $ParentEducation.\n".
-              "Gender: $Gender.\n".
-              "applying For Std.: $Applyingfor.\n".
+$email_body = "Father's Name: $Father_Name.\n".
+              "Mother's Name: $Mother_Name.\n".
+              "Student Name: $Student_Name.\n".
+              "DOB: $Dob.\n".
+              "Parent's Education = $Parent_Education.\n".
+              "Gender: $Gender_.\n".
+              "applying For Std.: $Applying_for.\n".
               "Aadhar No.: $AadharNo.\n".
               "Phone No.: $PhoneNo.\n".
               "Email Id: $visitor_email.\n".
-              "Address: $Address.\n".
+              "Address: $Address_.\n".
 
               $to = "sdmpsnaigaon@gmail.com";
 
               $headers = "From: $email_from \r\n".
-              $headers = "Reply-To: $visitor_email\r\n".
+              $headers .= "Reply-To: $visitor_email\r\n".
               
               mail($to,$email_subject,$email_body,$headers)
               
-              header("location: application.html")
+              header("Location: application.html")
 
 
 ?>
